4a49e6085459932fc8a4a32438071fa200ea7514
Morris Kimani <20963766+morriskimani@users.noreply.github.com>
2021-07-16 09:09:49 +0300

    add connection handlers

--- lib/elarian/client.rb
+++ lib/elarian/client.rb
@@ -3,8 +3,9 @@
 require_relative "./utils/custom_load_paths"
 require "app_socket_pb"
 require "google/protobuf"
-require "rsocket/requester"
 require "eventmachine"
+require_relative 'requester'
+
 module Elarian
   class Client
     EXPECTED_EVENTS = %w[pending error connecting connected closed].freeze
@@ -37,14 +38,14 @@ module Elarian
     end
 
     def on(event, handler)
-      raise ArgumentError, "Unrecognized event (#{event})" unless EXPECTED_EVENTS.include?(event&.to_s)
-      raise ArgumentError, "Invalid handler provided. Handler must be callable." unless handler.respond_to?(:call)
+      raise ArgumentError("Unrecognized event (#{event})") unless EXPECTED_EVENTS.include?(event&.to_s)
+      raise ArgumentError("Invalid handler provided. Handler must be callable.") unless handler.respond_to?(:call)
 
       @handlers[event.to_sym] = handler
     end
 
     def send_command(data)
-      raise "Client is not connected" unless connected?
+      raise RuntimeError("Client is not connected") unless connected?
 
       @socket.request_response(payload_of(data.to_proto, nil))
     end
