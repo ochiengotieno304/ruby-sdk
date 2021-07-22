3d20b64c3d040f1a6ea347656ea7e01845d2e02f
Morris Kimani <20963766+morriskimani@users.noreply.github.com>
2021-07-22 12:43:08 +0300

    delete duplicate methods

--- lib/elarian/utils/utils.rb
+++ lib/elarian/utils/utils.rb
@@ -41,32 +41,6 @@ module Elarian
           raise ArgumentError, "Invalid #{object_name} type. Expected #{expected_type} got #{object.class}"
         end
       end
-
-      def get_enum_value(target, key, prefix)
-        target.const_get("#{prefix}_#{key}".upcase, false)
-      rescue NameError
-        raise ArgumentError, "Invalid key #{key.inspect}. Valid keys are #{valid_enum_keys(target, prefix)}"
-      end
-
-      def valid_enum_keys(target, prefix)
-        target.constants.reject { |c| c == :UNSPECIFIED || c == :UNKNOWN }
-          .map { |c| c.to_s.split("#{prefix}_").last.to_sym }
-      end
-
-      def assert_keys_present(hash, required_keys, hash_name = "Hash")
-        assert_type(hash, hash_name, Hash)
-        required_keys.each do |key|
-          unless hash.key?(key)
-            raise ArgumentError, "#{hash_name} missing one or more required keys. Required keys are: #{required_keys}"
-          end
-        end
-      end
-
-      def assert_type(object, object_name, expected_type)
-        unless object.is_a? expected_type
-          raise ArgumentError, "Invalid #{object_name} type. Expected #{expected_type} got #{object.class}"
-        end
-      end
     end
   end
 end
