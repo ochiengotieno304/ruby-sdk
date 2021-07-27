86c537337e0beb43744e76267e3caf415dd3a099
Morris Kimani <20963766+morriskimani@users.noreply.github.com>
2021-07-27 08:34:44 +0300

    Add methods #generate_auth_token, #cancel_customer_reminder_by_tag, #send_message_by_tag

--- lib/elarian/elarian.rb
+++ lib/elarian/elarian.rb
@@ -2,7 +2,7 @@
 
 module Elarian
   class Elarian < Client
-    attr_reader :client
+    include ::Elarian::AppToServerCommandsHelper
 
     def initialize(org_id:, app_id:, api_key:, options: {})
       events = %i[
@@ -26,6 +26,46 @@ module Elarian
         customer_activity
       ]
       super(org_id: org_id, app_id: app_id, api_key: api_key, options: options, events: events)
+    end
+
+    def generate_auth_token
+      command = P::GenerateAuthTokenCommand.new
+      send_command(:generate_auth_token, command)
+    end
+
+    def cancel_customer_reminder_by_tag(key, tag)
+      Utils.assert_type(key, "key", String)
+      Utils.assert_type(tag, "tag", Hash)
+
+      command = P::CancelCustomerReminderTagCommand.new(
+        key: key,
+        tag: { key: tag[:key], value: { value: tag[:value] } }
+      )
+      send_command(:cancel_customer_reminder_tag, command)
+    end
+
+    def send_message_by_tag(tag, messaging_channel, message)
+      { tag: tag, messaging_channel: messaging_channel, message: message }.each do |name, value|
+        Utils.assert_type(value, name, Hash)
+      end
+
+      channel = Utils.get_enum_value(
+        P::MessagingChannel, messaging_channel.fetch(:channel, "UNSPECIFIED"), "MESSAGING_CHANNEL"
+      )
+
+      command = P::SendMessageTagCommand.new(
+        channel_number: P::MessagingChannelNumber.new(number: messaging_channel[:number], channel: channel),
+        tag: { key: tag[:key], value: { value: tag[:value] } },
+        message: Utils::OutgoingMessageSerializer.serialize(message)
+      )
+      send_command(:send_message_tag, command)
+    end
+
+    private
+
+    # Used by some helper methods included in this class
+    def client
+      self
     end
   end
 end
