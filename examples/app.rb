f6797de43237b3a53b534d08139340c47f3dc209
Victor Nguli <victornjoseph@gmail.com>
2021-08-09 12:27:05 +0300

    style and logic fixes on example app

--- examples/app.rb
+++ examples/app.rb
@@ -23,14 +23,13 @@ def approve_loan(customer, amount)
   puts "Approving loan for #{customer.number[:number]}"
   customer.get_metadata
           .flat_map do |meta|
-            meta = meta.to_h
             name = meta.fetch("name", "Unknown Customer")
             repayment_date = Time.now + 60
 
             init_payment = @client.initiate_payment(
-              { purse: { purse_id: @purse_id } },
-              { customer: { channel_number: @mpesa_channel, customer_number: customer.number } },
-              { amount: amount, currency_code: "KES" }
+              debit_party: { purse: { purse_id: @purse_id } },
+              credit_party: { customer: { channel_number: @mpesa_channel, customer_number: customer.number } },
+              value: { amount: amount, currency_code: "KES" }
             )
             init_payment.flat_map do |res|
               raise "Failed to make loan payment: #{res}" unless %w[SUCCESS PENDING_CONFIRMATION].include?(res[:status])
@@ -53,7 +52,6 @@ def handle_payment(notification, customer, _app_data)
   puts "Processing payment from #{notification[:customer_number][:number]}"
   customer.get_metadata
           .flat_map do |meta|
-            meta = meta.to_h
             amount = notification[:value][:amount]
             name = meta.fetch("name", "Unknown Customer")
             balance = meta.fetch("balance", 0).to_f
@@ -61,12 +59,12 @@ def handle_payment(notification, customer, _app_data)
             new_balance = balance - amount
             customer.update_metadata({ "name": name, "balance": new_balance })
                     .flat_map do
-                      if new_balance.negative?
+                      if new_balance.positive?
+                        text = "Hey #{name}!\nThank you for your payment, but you still owe me KES #{new_balance}"
+                      else
                         text = "Thank you for your payment #{name}, your loan has been fully repaid!!"
                         customer.cancel_reminder("moni")
                                 .flat_map { customer.delete_metadata(%w[name strike balance screen]) }
-                      else
-                        text = "Hey #{name}!\nThank you for your payment, but you still owe me KES #{new_balance}"
                       end
                       customer.send_message(@sms_channel, { body: { text: text } })
                     end
@@ -84,65 +82,62 @@ def handle_ussd(notification, customer, app_data)
   screen = app_data.fetch(:screen, "home")
 
   customer.get_metadata
-          .flat_map do |meta|
-            meta = meta.to_h
-            name = meta["name"]
-            balance = meta.fetch("balance", 0)
-            menu = { text: nil, is_terminal: false }
-
-            next_screen = screen
-            if screen == "home" && ussd_input != ""
-              case ussd_input
-              when "1"
-                next_screen = "request-name"
-              when "2"
-                next_screen = "quit"
-              end
-            end
-
-            next_screen = "info" unless name.nil? && screen == "home" && ussd_input == ""
-            case next_screen
-            when "quit"
-              menu["text"] = "Happy Coding!"
-              menu["is_terminal"] = true
-              next_screen = "home"
-              yield({ body: { ussd: menu } }, { screen: next_screen })
-            when "info"
-              menu["text"] = if balance.positive?
-                               "Hey #{name} you still owe me KES #{balance}"
-                             else
-                               "Hey #{name} you have repaid your loan, good for you!"
-                             end
-              menu["is_terminal"] = true
-              next_screen = "home"
-              yield({ body: { ussd: menu } }, { screen: next_screen })
-            when "request-name"
-              menu["text"] = "Alright, what is your name?"
-              next_screen = "request-amount"
-              yield({ body: { ussd: menu } }, { screen: next_screen })
-            when "request-amount"
-              name = ussd_input
-              menu["text"] = "Okay #{name}, how much do you need?"
-              next_screen = "approve-amount"
-              yield({ body: { ussd: menu } }, { screen: next_screen })
-            when "approve-amount"
-              balance = ussd_input.to_f
-              menu["text"] = "Awesome! #{name} we are reviewing your application
-                              and will be in touch shortly!\nHave a lovely day!"
-              menu["is_terminal"] = true
-              next_screen = "home"
-              yield({ body: { ussd: menu } }, { screen: next_screen })
-              @client.approve_loan(customer, balance)
-            when "home"
-              menu["text"] = "Welcome to MoniMoni!\n1. Apply for loan\n2. Quit"
-              menu["is_terminal"] = false
-              yield({ body: { ussd: menu } }, { screen: next_screen })
-            end
-
-            customer.update_metadata({ name: name, balance: balance })
-          end
           .subscribe(
-            ->(_res) { puts "Successfully Processed ussd request from #{notification[:customer_number][:number]}" },
+            lambda(meta) do
+              name = meta.fetch("name", "Unknown Customer")
+              balance = meta.fetch("balance", 0)
+              menu = { text: nil, is_terminal: false }
+
+              next_screen = screen
+              if screen == "home" && ussd_input != ""
+                case ussd_input
+                when "1"
+                  next_screen = "request-name"
+                when "2"
+                  next_screen = "quit"
+                end
+              end
+
+              next_screen = "info" if !name.nil? && screen == "home" && ussd_input == ""
+              case next_screen
+              when "quit"
+                menu["text"] = "Happy Coding!"
+                menu["is_terminal"] = true
+                next_screen = "home"
+                yield({ body: { ussd: menu } }, { screen: next_screen })
+              when "info"
+                menu["text"] = if balance.positive?
+                                 "Hey #{name} you still owe me KES #{balance}"
+                               else
+                                 "Hey #{name} you have repaid your loan, good for you!"
+                               end
+                menu["is_terminal"] = true
+                next_screen = "home"
+                yield({ body: { ussd: menu } }, { screen: next_screen })
+              when "request-name"
+                menu["text"] = "Alright, what is your name?"
+                next_screen = "request-amount"
+                yield({ body: { ussd: menu } }, { screen: next_screen })
+              when "request-amount"
+                name = ussd_input
+                menu["text"] = "Okay #{name}, how much do you need?"
+                next_screen = "approve-amount"
+                yield({ body: { ussd: menu } }, { screen: next_screen })
+              when "approve-amount"
+                balance = ussd_input.to_f
+                menu["text"] = "Awesome! #{name} we are reviewing your application
+                                and will be in touch shortly!\nHave a lovely day!"
+                menu["is_terminal"] = true
+                next_screen = "home"
+                yield({ body: { ussd: menu } }, { screen: next_screen })
+                approve_loan(customer, balance)
+              when "home"
+                menu["text"] = "Welcome to MoniMoni!\n1. Apply for loan\n2. Quit"
+                menu["is_terminal"] = false
+                yield({ body: { ussd: menu } }, { screen: next_screen })
+              end
+              puts "Successfully Processed ussd request from #{customer.number[:number]}"
+            end,
             ->(err) { puts "Failed to process ussd: #{err}" },
             -> {}
           )
@@ -152,7 +147,6 @@ def handle_reminder(_notification, customer, _app_data)
   puts "Processing Reminder"
   customer.get_metadata
           .flat_map do |meta|
-            meta = meta.to_h
             name = meta.fetch("name", "Unknown Customer")
             balance = meta.fetch("balance", 0).to_f
             strike = meta.fetch("strike", 1)
@@ -183,12 +177,13 @@ def handle_reminder(_notification, customer, _app_data)
           )
 end
 
+@cust = Elarian::Customer.new(client: @client, number: "0710005123")
+
 @client.on("connected", -> { puts "App is connected, waiting for customers on #{ENV["USSD_CODE"]}" })
 @client.on("closed", -> { puts "Connection Closed" })
-@client.on("received_payment", ->(*args, &blk) { handle_payment(*args, &blk) })
-@client.on("received_sms", ->(*args, &blk) { handle_sms(*args, &blk) })
-@client.on("reminder", ->(*args, &blk) { handle_reminder(*args, &blk) })
-@client.on("ussd_session", ->(*args, &blk) { handle_ussd(*args, &blk) })
+@client.on("received_payment", method(:handle_payment))
+@client.on("reminder", method(:handle_reminder))
+@client.on("ussd_session", method(:handle_ussd))
 
 EventMachine.run do
   @client.connect
